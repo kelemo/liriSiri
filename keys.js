0712ce80818ea5f28d7cfd5e37a932a6ce459945
Kadiro Elemo <you@example.com>
2018-02-07 18:02:05 -0600

    LiriSiri homework

---
+++ keys.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+
+    consumer_key:         'mmImiEODl5HqR7oAGzxRw0m0W'
+  , consumer_secret:      'wIRcgVRd1XpEBU4t4amyLxxJVHxhyDNbfq7i5kksLg72wdEm0F'
+  , access_token:         '960994752895348737-7j9wEDuP2RpNSiSifQeJHk9ybiRq4lV'
+  , access_token_secret:  'lXDvTyHsFlg6FcPedU0GSj8DfVfWD2cWKbS4fNYzKmi0z'
+
+
+}
---
+++ liri.js
@@ -0,0 +1,161 @@
+// Here we different varibales and require data from keys.js
+
+var keys = require('./keys.js');
+console.log(keys);
+var request = require('request');
+var twit = require('twit');
+var spotify = require('spotify');
+var client = new twitter(keys.twitterKeys);
+var fs = require('fs');
+
+// This will enable us to put in arguments and store in the array.
+
+var value = process.argv[2];
+var action= process.argv[3];
+
+// Create a movie or song search and store in a variable x and loop through the length of the value array.
+
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<value.length; i++){
+  if(i>3 && i<value.length){
+    x = x + "+" + value[i];
+  } else{
+    x = x + value[i];
+  }
+}
+
+/* Switch case will function the same way like putting different conditional statements in JS. 
+If this is met, display this or if not, display a different thing. In this case, the display options are among 
+twit, spotify and omdb*/
+
+switch(action){
+  case "my-tweets":
+    showTweets();
+  break;
+
+  case "spotify-this-song":
+    if(x){
+      spotifySong(x);
+    } else{
+      spotifySong("Fluorescent Adolescent");
+    }
+  break;
+
+  case "movie-this":
+    if(x){
+      omdbData(x)
+    } else{
+      omdbData("Mr. Nobody")
+    }
+  break;
+
+  case "do-what-it-says":
+    doThing();
+  break;
+
+  default:
+    console.log("{Please enter an action: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+  break;
+}
+
+function showTweets(){
+  //Display last 20 Tweets
+  var screenName = {screen_name: 'stefanieding'};
+  client.get('statuses/user_timeline', screenName, function(error, tweets, response){
+    if(!error){
+      for(var i = 0; i<tweets.length; i++){
+        var date = tweets[i].created_at;
+        console.log("@StefanieDing: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log("-----------------------");
+        
+        //adds text to log.txt file
+        fs.appendFile('log.txt', "@StefanieDing: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        fs.appendFile('log.txt', "-----------------------");
+      }
+    }else{
+      console.log('Error occurred');
+    }
+  });
+}
+
+function spotifySong(song){
+  spotify.search({ type: 'track', query: song}, function(error, data){
+    if(!error){
+      for(var i = 0; i < data.tracks.items.length; i++){
+        var songData = data.tracks.items[i];
+        //artist
+        console.log("Artist: " + songData.artists[0].name);
+        //song name
+        console.log("Song: " + songData.name);
+        //spotify preview link
+        console.log("Preview URL: " + songData.preview_url);
+        //album name
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------");
+        
+        //adds text to log.txt
+        fs.appendFile('log.txt', songData.artists[0].name);
+        fs.appendFile('log.txt', songData.name);
+        fs.appendFile('log.txt', songData.preview_url);
+        fs.appendFile('log.txt', songData.album.name);
+        fs.appendFile('log.txt', "-----------------------");
+      }
+    } else{
+      console.log('Error occurred.');
+    }
+  });
+}
+
+function omdbData(movie){
+  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
+
+  request(omdbURL, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "Title: " + body.Title);
+      fs.appendFile('log.txt', "Release Year: " + body.Year);
+      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
+      fs.appendFile('log.txt', "Country: " + body.Country);
+      fs.appendFile('log.txt', "Language: " + body.Language);
+      fs.appendFile('log.txt', "Plot: " + body.Plot);
+      fs.appendFile('log.txt', "Actors: " + body.Actors);
+      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
+      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else{
+      console.log('Error occurred.')
+    }
+    if(movie === "Mr. Nobody"){
+      console.log("-----------------------");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!");
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "-----------------------");
+      fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      fs.appendFile('log.txt', "It's on Netflix!");
+    }
+  });
+
+}
+
+function doThing(){
+  fs.readFile('random.txt', "utf8", function(error, data){
+    var txt = data.split(',');
+
+    spotifySong(txt[1]);
+  });
+}
